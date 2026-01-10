4a7e75509c94ad7d5088cfcf63a51585889525da
Ubuntu <ubuntu@ip-172-31-37-194.eu-west-1.compute.internal>
2026-01-10 18:48:10 +0000

    update main.tf globale plan fonctionne

--- serverlessArchi_terraform/main.tf
+++ serverlessArchi_terraform/main.tf
@@ -6,3 +6,21 @@ terraform {
     }
   }
 }
+
+provider "aws" {
+  region = "eu-west-3" # Paris
+}
+
+module "my_lambda" {
+  source = "./lambdaFunction"
+}
+
+module "my_apigw" {
+  source               = "./apigw"
+  lambda_invoke_arn    = module.my_lambda.lambda_invoke_arn
+  lambda_function_name = module.my_lambda.lambda_function_name
+}
+
+output "final_url" {
+  value = "${module.my_apigw.invoke_url}/serverless-demo-path"
+}
