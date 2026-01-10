b544a36710daa8c2daa0fa28865bace921dc7feb
Ubuntu <ubuntu@ip-172-31-37-194.eu-west-1.compute.internal>
2026-01-10 19:16:12 +0000

    ajout de dependance entre les resources

--- serverlessArchi_terraform/apigw/main.tf
+++ serverlessArchi_terraform/apigw/main.tf
@@ -1,56 +1,67 @@
 resource "aws_api_gateway_rest_api" "rest_api" {
   name = "rest_api"
-
   endpoint_configuration {
-    types = [ "REGIONAL" ]
+    types = ["REGIONAL"]
   }
 }
 
 resource "aws_api_gateway_resource" "api_resource" {
-    parent_id = aws_api_gateway_rest_api.rest_api.root_resource_id
-    path_part = "serverless-demo-path"
-    rest_api_id = aws_api_gateway_rest_api.rest_api.id
-  
+  parent_id   = aws_api_gateway_rest_api.rest_api.root_resource_id
+  path_part   = "serverless-demo-path"
+  rest_api_id = aws_api_gateway_rest_api.rest_api.id
 }
 
 resource "aws_api_gateway_method" "api_method" {
-  authorization = "None"
-  http_method = "POST"
-  resource_id = aws_api_gateway_resource.api_resource.id
-  rest_api_id = aws_api_gateway_rest_api.rest_api.id
+  authorization = "NONE" # Attention : "NONE" en majuscule est plus standard
+  http_method   = "POST"
+  resource_id   = aws_api_gateway_resource.api_resource.id
+  rest_api_id   = aws_api_gateway_rest_api.rest_api.id
 }
 
 resource "aws_api_gateway_integration" "lambda_integration" {
-  http_method = aws_api_gateway_method.api_method.http_method
-  resource_id = aws_api_gateway_resource.api_resource.id
-  rest_api_id = aws_api_gateway_rest_api.rest_api.id
-  type = "AWS_PROXY"
+  http_method             = aws_api_gateway_method.api_method.http_method
+  resource_id             = aws_api_gateway_resource.api_resource.id
+  rest_api_id             = aws_api_gateway_rest_api.rest_api.id
+  type                    = "AWS_PROXY"
   integration_http_method = "POST"
-  uri = var.lambda_invoke_arn
-
+  uri                     = var.lambda_invoke_arn
 }
 
+# --- LA RESSOURCE CRITIQUE ---
 resource "aws_api_gateway_deployment" "api_deployment" {
   rest_api_id = aws_api_gateway_rest_api.rest_api.id
+
+  # Dépendance explicite indispensable :
+  # On ne déploie QUE si la méthode et l'intégration sont totalement finies
+  depends_on = [
+    aws_api_gateway_method.api_method,
+    aws_api_gateway_integration.lambda_integration
+  ]
 
   lifecycle {
     create_before_destroy = true
   }
 
+  # Astuce d'expert : Forcer le redéploiement si la config change
+  triggers = {
+    redeployment = sha1(jsonencode([
+      aws_api_gateway_resource.api_resource.id,
+      aws_api_gateway_method.api_method.id,
+      aws_api_gateway_integration.lambda_integration.id,
+    ]))
+  }
 }
 
 resource "aws_api_gateway_stage" "api_stage" {
   deployment_id = aws_api_gateway_deployment.api_deployment.id
-  rest_api_id = aws_api_gateway_rest_api.rest_api.id
-  stage_name = "dev"
+  rest_api_id   = aws_api_gateway_rest_api.rest_api.id
+  stage_name    = "dev"
 }
 
 resource "aws_lambda_permission" "lambda_permission" {
-    principal = "apigateway.amazonaws.com"
-    statement_id = "AllowExecutionFromAPIGateway"
-    action = "lambda:InvokeFunction"
-    function_name = var.lambda_function_name
-    source_arn    = "${aws_api_gateway_rest_api.rest_api.execution_arn}/*/*/*"
-
+  principal     = "apigateway.amazonaws.com"
+  statement_id  = "AllowExecutionFromAPIGateway"
+  action        = "lambda:InvokeFunction"
+  function_name = var.lambda_function_name
+  source_arn    = "${aws_api_gateway_rest_api.rest_api.execution_arn}/*/*/*"
 }
-
