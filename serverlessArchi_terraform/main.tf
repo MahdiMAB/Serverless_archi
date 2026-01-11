76b2096972d45a5b1a8ffbf1faa9fa1dc773a996
Ubuntu <ubuntu@ip-172-31-37-194.eu-west-1.compute.internal>
2026-01-11 11:38:04 +0000

    update de main.tf globale pour rajouter les modules cognito et cloudwatch

--- serverlessArchi_terraform/main.tf
+++ serverlessArchi_terraform/main.tf
@@ -1,11 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "4.47.0"
-    }
-  }
-}
 
 provider "aws" {
   region = "eu-west-3" # Paris
@@ -15,11 +7,27 @@ module "my_lambda" {
   source = "./lambdaFunction"
 }
 
+
+
+
 module "my_apigw" {
-  source               = "./apigw"
-  lambda_invoke_arn    = module.my_lambda.lambda_invoke_arn
-  lambda_function_name = module.my_lambda.lambda_function_name
+  source                    = "./apigw"
+  lambda_invoke_arn          = module.my_lambda.lambda_invoke_arn
+  lambda_function_name       = module.my_lambda.lambda_function_name
+  cognito_user_pool_arn      = module.cognito.user_pool_arn
 }
+
+module "cognito" {
+  source = "./cognito"
+}
+
+module "cloudwatch" {
+  source               = "./cloudwatch"
+  lambda_function_name = module.my_lambda.lambda_function_name
+  retention_days       = 30
+  environment           = "dev"
+}
+
 
 output "final_url" {
   value = "${module.my_apigw.invoke_url}/serverless-demo-path"
