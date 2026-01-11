9e2c23e490d8e894a98c37040650acb89b44dbb7
Ubuntu <ubuntu@ip-172-31-37-194.eu-west-1.compute.internal>
2026-01-11 11:36:37 +0000

    ajouter le module cognito

---
+++ serverlessArchi_terraform/cognito/main.tf
@@ -0,0 +1,25 @@
+resource "aws_cognito_user_pool" "user_pool" {
+  name = "safran-user-pool"
+
+  auto_verified_attributes = ["email"]
+
+  password_policy {
+    minimum_length    = 8
+    require_lowercase = true
+    require_numbers   = true
+    require_symbols   = false
+    require_uppercase = true
+  }
+}
+
+resource "aws_cognito_user_pool_client" "app_client" {
+  name         = "safran-app-client"
+  user_pool_id = aws_cognito_user_pool.user_pool.id
+
+  generate_secret = false
+
+  explicit_auth_flows = [
+    "ALLOW_USER_PASSWORD_AUTH",
+    "ALLOW_REFRESH_TOKEN_AUTH"
+  ]
+}
